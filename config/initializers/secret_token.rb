fd8ebcf7e735e7bc3d37a1854905c1f8cab8040b
LxSwiss <lxswiss@gmail.com>
2013-08-27 20:45:48 +0300

    Alpacca
    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '999c1d47ed4f63b68492062a9bb2b1a34506afe6cffbf22ad67caabfebef12620f628e83c0bf29673dd4ac89b08d401f12a1e7e48fff2d004fa9c24d03cb26b8'
